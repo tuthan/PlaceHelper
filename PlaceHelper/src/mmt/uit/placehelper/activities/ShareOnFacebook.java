d1fc6b17c9285e202483d5148ab0dbe2123fd0fd
Hungdohuy <Hungdohuy@gmail.com>
2012-02-29 01:55:57 +0700

    Change log: add function share to facebook

---
+++ PlaceHelper/src/mmt/uit/placehelper/activities/ShareOnFacebook.java
@@ -0,0 +1,132 @@
+package mmt.uit.placehelper.activities;
+import mmt.uit.placehelper.R;
+
+import com.facebook.android.*;
+import com.facebook.android.Facebook.DialogListener;
+
+import android.app.*;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.util.*;
+import android.view.View;
+import android.view.Window;
+import android.widget.Toast;
+
+public class ShareOnFacebook extends Activity{
+
+	private static final String APP_ID = "325154344184854";
+	private static final String[] PERMISSIONS = new String[] {"publish_stream"};
+
+	private static final String TOKEN = "access_token";
+        private static final String EXPIRES = "expires_in";
+        private static final String KEY = "facebook-credentials";
+
+	private Facebook facebook;
+	private String messageToPost;
+	private String Name;
+
+	public boolean saveCredentials(Facebook facebook) {
+        	Editor editor = getApplicationContext().getSharedPreferences(KEY, Context.MODE_PRIVATE).edit();
+        	editor.putString(TOKEN, facebook.getAccessToken());
+        	editor.putLong(EXPIRES, facebook.getAccessExpires());
+        	return editor.commit();
+    	}
+
+    	public boolean restoreCredentials(Facebook facebook) {
+        	SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(KEY, Context.MODE_PRIVATE);
+        	facebook.setAccessToken(sharedPreferences.getString(TOKEN, null));
+        	facebook.setAccessExpires(sharedPreferences.getLong(EXPIRES, 0));
+        	return facebook.isSessionValid();
+    	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		facebook = new Facebook(APP_ID);
+		restoreCredentials(facebook);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		setContentView(R.layout.facebook_dialog);
+		Bundle bundle = getIntent().getExtras();
+		String URL = bundle.getString("URL");
+		String name = bundle.getString("Name");
+
+		String facebookMessage = getIntent().getStringExtra("facebookMessage");
+		if (facebookMessage == null){
+			facebookMessage = URL;
+		}
+		messageToPost = facebookMessage;
+		Name = name;
+	}
+
+	public void doNotShare(View button){
+		finish();
+	}
+	public void share(View button){
+		if (! facebook.isSessionValid()) {
+			loginAndPostToWall();
+		}
+		else {
+			postToWall(messageToPost, Name);
+		}
+	}
+
+	public void loginAndPostToWall(){
+		 facebook.authorize(this, PERMISSIONS, Facebook.FORCE_DIALOG_AUTH, new LoginDialogListener());
+	}
+
+
+	public void postToWall(String link, String name){
+		Bundle parameters = new Bundle();
+                parameters.putString("message", "Hãy xem địa điểm thú vị này:");
+                parameters.putString("name", name);
+                parameters.putString("link", link);
+                parameters.putString("description", "Khám phá nhiều hơn nữa với PlaceHelper!");
+                try {
+        	        facebook.request("me");
+			String response = facebook.request("me/feed", parameters, "POST");
+			Log.d("Tests", "got response: " + response);
+			if (response == null || response.equals("") || response.equals("false")) {
+				showToast("Blank response.");
+			}
+			else {
+				showToast("Message posted to your facebook wall!");
+			}
+			finish();
+		} catch (Exception e) {
+			showToast("Failed to post to wall!");
+			e.printStackTrace();
+			finish();
+		}
+	}
+
+	class LoginDialogListener implements DialogListener {
+	    public void onComplete(Bundle values) {
+	    	saveCredentials(facebook);
+	    	if (messageToPost != null){
+			postToWall(messageToPost, Name);
+		}
+	    }
+	    public void onFacebookError(FacebookError error) {
+	    	showToast("Authentication with Facebook failed!");
+	        finish();
+	    }
+	    public void onError(DialogError error) {
+	    	showToast("Authentication with Facebook failed!");
+	        finish();
+	    }
+	    public void onCancel() {
+	    	showToast("Authentication with Facebook cancelled!");
+	        finish();
+	    }
+	}
+
+	
+	private void showToast(String message){
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+	}
+}
